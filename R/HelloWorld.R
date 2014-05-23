07342f2240b4f63b10ff7cb154a1b58369c4cf7d
Jessy LeClair <jessy.leclair@gmail.com>
2014-05-23 16:55:23 -0700

    Adding barplot practice with full and subsetting data

--- R/HelloWorld.R
+++ R/HelloWorld.R
@@ -1,3 +1,13 @@
 PrintGreeting <- function(greeting="Hello, World!") {
   print(greeting)
 }
+
+data <- read.csv("CompositeGenes2.csv", header = T)
+attach(data)
+
+mytable <- with(data, table(DRD4))
+barplot (mytable, main = "Simple Bar Plot", xlab = "DRD4", ylab = "Frequency")
+
+d <- subset(data, culture %in% c(1, 2), select = c(culture, DRD4))
+mytable.d <- with(d, table(DRD4))
+barplot (mytable.d, main = "Simple Bar Plot", xlab = "DRD4", ylab = "Frequency")
